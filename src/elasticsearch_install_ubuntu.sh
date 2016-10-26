ba9963dfc994ed421a258651dbeaf0882b4fc1d0
rst0git <rst0git@users.noreply.github.com>
2016-10-26 14:01:32 +0100

    Required Privileges

--- src/elasticsearch_install_ubuntu.sh
+++ src/elasticsearch_install_ubuntu.sh
@@ -3,8 +3,8 @@ sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get -y install oracle-java8-installer
 wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.4.1/elasticsearch-2.4.1.deb
-dpkg -i elasticsearch-2.4.1.deb
-service elasticsearch start
+sudo dpkg -i elasticsearch-2.4.1.deb
+sudo service elasticsearch start
 clear
 echo "--------------------------------------------------"
 echo "Elasticsearch is running on http://localhost:9200"
