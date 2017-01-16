2254b7247d5586d23abe997c19df999e674128a2
rst0git <rstoyanov1@gmail.com>
2017-01-16 08:15:24 +0000

    Added description to xml_to_json.rb

--- src/lacr-demo/xml-to-json.rb
+++ src/lacr-demo/xml-to-json.rb
@@ -4,7 +4,7 @@
 #     * goes through each '.xml' document in the directory 'app/assests/transcriptions'
 #     * reads the content of the file into the variable 'doc'
 #     * for each 'div' tag with key: 'type'='court'
-#         * extract the xml keys ( e.g "when"=>"1457-01-29", "cert"=>"high") of the 'data' xml tag into variable 'date'
+#         * extract the xml keys ( e.g "when"=>"1457-01-29", "cert"=>"high") of the 'date' xml tag into variable 'date'
 #         * for each 'div' tag which is a child of the above 'div' tag with 'type'='court'
 #             * extract the keys (e.g "type"=>"entry", "xml:id"=>"ARO-5-0294-11", "xml:lang"=>"la") into variable 'metadata'
 #             * extract the text content into variable 'data'
