17f8053575681f0ca3d24d979503ea853387584d
rst0git <rstoyanov1@gmail.com>
2017-01-20 07:54:55 +0000

    Created containers for Elasticsearch, Redis, Postgresql and Rails. To run execute ./start

--- src/lacr-demo/config/application.rb
+++ src/lacr-demo/config/application.rb
@@ -6,10 +6,31 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# module LacrDemo
+#   class Application < Rails::Application
+#     # Settings in config/environments/* take precedence over those specified here.
+#     # Application configuration should go into files in config/initializers
+#     # -- all .rb files in that directory are automatically loaded.
+#   end
+# end
+
 module LacrDemo
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # We want to set up a custom logger which logs to STDOUT.
+    # Docker expects your application to log to STDOUT/STDERR and to be ran
+    # in the foreground.
+    config.log_level = :debug
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(STDOUT))
+
+    # Since we're using Redis for Sidekiq, we might as well use Redis to back
+    # our cache store. This keeps our application stateless as well.
+    config.cache_store = :redis_store, ENV['CACHE_URL'],
+                         { namespace: 'lacrdemo::cache' }
+
+    # If you've never dealt with background workers before, this is the Rails
+    # way to use them through Active Job. We just need to tell it to use Sidekiq.
+    config.active_job.queue_adapter = :sidekiq
+
   end
 end
