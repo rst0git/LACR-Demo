7b9c165c035e1d9544c6dd0340cba7ce4af309ce
rst0git <rstoyanov1@gmail.com>
2017-02-11 14:51:42 +0000

    Fix image filename spliting into volume and page number: Volume is always the first valid integer in the name where the Page is always the last valid integer.

--- src/lacr-demo/app/models/page_image.rb
+++ src/lacr-demo/app/models/page_image.rb
@@ -3,6 +3,11 @@ class PageImage < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   def parse_filename_to_volume_page(filename)
-    self.volume, self.page = filename.split(/-|_/).map{|x| x.to_i}.delete_if{|i| i==0}
+    # Split the file name into array of valid integers
+    arr = filename.split(/-|_/).map{|x| x.to_i}.delete_if{|i| i<=0}
+
+    # Set the volume number to be the first valid integer
+    # and the page number to be the last valid integer
+    self.volume, self.page = arr[0], arr[-1]
   end
 end
