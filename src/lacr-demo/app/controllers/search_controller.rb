76e96ab031e1030be5fa459f34f99439ebf2eb9c
rst0git <rstoyanov1@gmail.com>
2017-02-14 06:26:39 +0000

    Fix for the mockup of the advanced search

--- src/lacr-demo/app/controllers/search_controller.rb
+++ src/lacr-demo/app/controllers/search_controller.rb
@@ -54,21 +54,52 @@ class SearchController < ApplicationController
     if Search.count.zero? # Fix search on empty table error msg
       redirect_to doc_path
     end
-    @content_all = params[:content_all].present? ? params[:content_all].split(' ') : ''
-    @content_any = params[:content_any].present? ? params[:content_any].split(' ') : ''
-    @content_none = params[:content_none].present? ? params[:content_none].split(' ') : ''
-    @documents = Search.search @query,
-        suggest: true,
-        match: :word_start,
-        page: params[:page], per_page: 6,
-        highlight: {tag: "<mark>"},
-        load: false,
-        misspellings: {prefix_length: 2},
-        where: {
-          content: @content_any,
-          content: {all: @content_all},
-          content: {not: @content_none}
-        }
+    # @content_all = params[:content_all].present? ? params[:content_all].split(' ') : ''
+    # @content_any = params[:content_any].present? ? params[:content_any].split(' ') : ''
+    # @content_none = params[:content_none].present? ? params[:content_none].split(' ') : ''
+    # @documents = Search.search @query,
+    #     suggest: true,
+    #     match: :word_start,
+    #     page: params[:page], per_page: 6,
+    #     highlight: {tag: "<mark>"},
+    #     load: false,
+    #     misspellings: {prefix_length: 2},
+    #     where: {
+    #       content: @content_any,
+    #       content: {all: @content_all},
+    #       content: {not: @content_none}
+    #     }
+        @query = params[:content_all].present? ? params[:content_all] : '*'
+
+        if params[:r].present?
+          if params[:r].to_i >= 5 and params[:r].to_i <= 50
+            @results_per_page = params[:r].to_i
+          else
+            @results_per_page = 5
+          end
+        else
+          @results_per_page = 5
+        end
+
+        if params[:m].present?
+          if params[:m].to_i >= 0 and params[:m].to_i <= 5
+            @misspellings = params[:m].to_i
+          else
+            @misspellings = 2
+          end
+        else
+          @misspellings = 2
+        end
+
+        @documents = Search.search @query,
+            fields: ['content'],
+            suggest: true,
+            match: :word_start,
+            page: params[:page], per_page: @results_per_page,
+            highlight: {tag: "<mark>"},
+            load: false,
+            misspellings: {edit_distance: @misspellings}
+
     render :search
   end
 end
