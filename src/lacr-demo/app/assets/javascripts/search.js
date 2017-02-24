aa5b637bff63eb6a0c8c3502bab14c44961ff56e
rst0git <rstoyanov1@gmail.com>
2017-02-24 20:11:50 +0000

    Enabled loading page from search result  with slide navigation

--- src/lacr-demo/app/assets/javascripts/search.js
+++ src/lacr-demo/app/assets/javascripts/search.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-    // $('#fullpage').fullpage({
-    //     navigationTooltips: ['results', 'doc'],
-    // });
+    $('a').click(function(e) {e.preventDefault();
+       load_page($(this).attr('data-page'));
+     });
 });
+
+function load_page(url){
+  if ($.isFunction($.fn.fullpage.destroy)) {
+    // Disable the fullpage plugin
+    $.fn.fullpage.destroy('all');
+  }
+  // Load the page with Ajax
+  $('#result-container').load(url, function () {
+    // Load Fullpage.js
+    $('#fullpage').fullpage();
+  });
+}
