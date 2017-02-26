f1dbfee93571af05a378e678bbd4938cf9801344
rst0git <rstoyanov1@gmail.com>
2017-02-26 18:29:44 +0000

    Use simple transcription page to avoid fullpageJS problems with resizing; Add title

--- src/lacr-demo/app/assets/javascripts/search.js
+++ src/lacr-demo/app/assets/javascripts/search.js
@@ -1,16 +1,17 @@
-//= require jquery.zoom.min.js
-//= require prettify.js
-//= require xml2html.js
 //= require ISO_639_2.min.js
 
 $(document).ready(function() {
     $('a.result-link').click(function(e) {
       e.preventDefault();
-      load_page($(this).attr('data-page'));
+      var $this = $(this);
+      url = $this.attr('data-url');
+      page = $this.attr('data-page');
+      vol = $this.attr('data-vol');
+      load_page(url, page, vol);
      });
 });
 
-function load_page(url){
+function load_page(url, page, vol){
   if ($.isFunction($.fn.fullpage.destroy)) {
     // Disable the fullpage plugin
     $.fn.fullpage.destroy('all');
@@ -27,6 +28,17 @@ function load_page(url){
       verticalCentered: false,
       loopHorizontal: false,
     });
+
+    // Transform language codes
+    $(".pr-language").each(function() {
+      $(this).html(ISO_639_2[$(this).html()]['native'][0]);
+    });
+
+    // Update the Title
+    $('#doc-title').html("Volume: "+vol+" Page: "+page);
+
+    // Slide to the loaded transcription
     document.location.href = "#results/1";
+
   });
 }
