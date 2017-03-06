95f0328ddeaafa9c867b6205240e93ea4e88bfad
rst0git <rstoyanov1@gmail.com>
2017-03-06 06:31:18 +0000

    Use regex for Entry ID search

--- src/lacr-demo/app/controllers/search_controller.rb
+++ src/lacr-demo/app/controllers/search_controller.rb
@@ -76,68 +76,67 @@ class SearchController < ApplicationController
       redirect_to doc_path
     end
 
-    if params[:q].present? # Searching
-      @query = params[:q].present? ? params[:q] : '*'
+    # Use wildcard when no content was specified
+    @query = params[:q].present? ? params[:q] : '*'
 
-      if params[:r].present?
-        if params[:r].to_i >= 5 and params[:r].to_i <= 50
-          @results_per_page = params[:r].to_i
-        else
-          @results_per_page = 5
-        end
+    if params[:r].present?
+      if params[:r].to_i >= 5 and params[:r].to_i <= 50
+        @results_per_page = params[:r].to_i
       else
         @results_per_page = 5
       end
+    else
+      @results_per_page = 5
+    end
 
-      if params[:m].present?
-        if params[:m].to_i >= 0 and params[:m].to_i <= 5
-          @misspellings = params[:m].to_i
-        else
-          @misspellings = 2
-        end
+    if params[:m].present?
+      if params[:m].to_i >= 0 and params[:m].to_i <= 5
+        @misspellings = params[:m].to_i
       else
         @misspellings = 2
       end
+    else
+      @misspellings = 2
+    end
 
-      where_query = {}
+    where_query = {}
 
-      if params[:entry]
-        where_query['entry'] = params[:entry]
-      end
+    if params[:entry]
+      where_query['entry'] = Regexp.new "#{params[:entry]}.*"
+    end
 
-      if params[:date_from]
-        where_query['date'] = {'gte': params[:date_from]}
-      end
+    if params[:date_from]
+      where_query['date'] = {'gte': params[:date_from]}
+    end
 
-      if params[:date_to]
-        where_query['date'] = {'lte': params[:date_to]}
-      end
+    if params[:date_to]
+      where_query['date'] = {'lte': params[:date_to]}
+    end
 
-      if params[:lang]
-        where_query['lang'] = params[:lang]
-      end
+    if params[:lang]
+      where_query['lang'] = params[:lang]
+    end
 
-      if params[:v]
-        where_query['volume'] = params[:v].split(/,| /).map { |s| s.to_i }
-      end
-      if params[:pg]
-        where_query['page'] = params[:pg].split(/,| /).map { |s| s.to_i }
-      end
-      if params[:pr]
-        where_query['paragraph'] = params[:pr].split(/,| /).map { |s| s.to_i }
-      end
+    if params[:v]
+      where_query['volume'] = params[:v].split(/,| /).map { |s| s.to_i }
+    end
+    if params[:pg]
+      where_query['page'] = params[:pg].split(/,| /).map { |s| s.to_i }
+    end
+    if params[:pr]
+      where_query['paragraph'] = params[:pr].split(/,| /).map { |s| s.to_i }
+    end
 
-      @documents = Search.search @query,
-      where: where_query,
-      fields: [:content],
-      highlight: {tag: "<mark>"},
-      suggest: true,
-      load: false,
-      page: params[:page], per_page: @results_per_page,
-      misspellings: {edit_distance: @misspellings}
+    @documents = Search.search @query,
+    where: where_query,
+    fields: [:content],
+    highlight: {tag: "<mark>"},
+    suggest: true,
+    load: false,
+    page: params[:page], per_page: @results_per_page,
+    misspellings: {edit_distance: @misspellings}
 
-      render :search
-    end # if params[:q].present?
+    render :search
   end # def advanced_search
 
 end
