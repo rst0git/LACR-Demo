72d10d49101c9fb85f14886110ab538753d00a1c
rst0git <rstoyanov1@gmail.com>
2017-03-06 07:14:55 +0000

    Added deselect all function and Download PDF button.

--- src/lacr-demo/app/assets/javascripts/documents_selected.js
+++ src/lacr-demo/app/assets/javascripts/documents_selected.js
@@ -4,6 +4,27 @@
 
 
 $(document).ready(function() {
+  // Deselect all
+  $('#deselect-all').click(function() {
+    noty({
+      text: '<h4 class="text-center">Would you like to deselect all entries ?</h4>',
+      layout: 'center',
+      buttons: [
+        { addClass: 'btn btn-default', text: 'Yes', onClick: function($noty) {
+            $noty.close();
+            Cookies.remove('selected_entries');
+            window.location.href="/doc";
+          }
+        },
+        { addClass: 'btn btn-default', text: 'Cancel',onClick: function($noty) { $noty.close(); }}
+      ]
+    });
+  });
+
+  // Download as PDF
+  $('#download-pdf').click(function() {
+    // TODO use prawn plugin to create PDF document
+  });
   // Transform language codes
   $(".pr-language").each(function() {
     $(this).html(ISO_639_2[$(this).html()]['native'][0]);
